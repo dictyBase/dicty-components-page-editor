a503ed1092a3fcbe6c1de1190725f641217c75ab
wildlifehexagon <crossthebreeze@gmail.com>
2019-09-27 16:12:10 -0500

    chore: add official fontawesome libraries

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react"
-
 import PageEditor from "dicty-components-page-editor"
-
+import data from "./data.json"
 export default class App extends Component {
   render() {
     return (
       <div>
-        <PageEditor />
+        <PageEditor
+          newPage
+          // pageContent={data}
+          onCancel={() => {}}
+          onSave={() => {}}
+          readOnly={false}
+        />
       </div>
     )
   }
