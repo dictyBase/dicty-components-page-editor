3c1419c6046b2591ad0515c30ff04eaae9405f16
wildlifehexagon <crossthebreeze@gmail.com>
2019-10-01 16:01:44 -0500

    refactor: use function component in example

--- example/src/App.js
+++ example/src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react"
+import React from "react"
 import { PageEditor } from "dicty-components-page-editor"
-import data from "./data.json"
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <PageEditor
-          newPage
-          // pageContent={data}
-          onCancel={() => {}}
-          onSave={() => {}}
-          readOnly={false}
-        />
-      </div>
-    )
-  }
+
+const App = () => {
+  return (
+    <div>
+      <PageEditor
+        newPage
+        // pageContent={data}
+        onCancel={() => {}}
+        onSave={() => {}}
+        readOnly={false}
+      />
+    </div>
+  )
 }
+
+export default App
