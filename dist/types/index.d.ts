db957d299ff425fd0405a66547e15035bf731802
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-29 14:28:42 -0600

    chore: remove old types

--- dist/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare type MarkFormat = "bold" | "italic" | "underline" | "strikethrough" | "subscript" | "superscript";
-declare type BlockFormat = "h1" | "h2";
-export type { MarkFormat, BlockFormat };
