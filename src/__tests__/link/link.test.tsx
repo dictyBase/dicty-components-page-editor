c876488663f47ff179e140d7053549414d4d1d16
wildlifehexagon <hartline.eric@gmail.com>
2021-03-05 20:25:13 -0600

    test: update test to fill out link dialog

--- src/__tests__/link/link.test.tsx
+++ src/__tests__/link/link.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import * as SlateReact from "slate-react"
 import LinkButton from "../../components/buttons/LinkButton"
@@ -8,19 +8,25 @@ import withLinks from "../../plugins/withLinks"
 import { input, output } from "./link.fixture"
 
 describe("adding links", () => {
-  it("should add new link", () => {
+  it("should add new link", async () => {
     const editor = withLinks(input)
 
     jest
       .spyOn(SlateReact, "useSlate")
       .mockReturnValue(editor as SlateReact.ReactEditor)
 
-    jest.spyOn(window, "prompt").mockReturnValue("https://dictycr.org")
-
     render(<LinkButton icon={<LinkIcon />} />)
 
     const button = screen.getByLabelText("link-button")
     userEvent.click(button)
-    expect(editor.children).toEqual(output.children)
+    const urlTextbox = screen.getAllByRole("textbox")[0]
+    const textTextbox = screen.getAllByRole("textbox")[1]
+    const addButton = screen.getByRole("button", { name: "Add Link" })
+    userEvent.type(urlTextbox, "https://dictycr.org")
+    userEvent.type(textTextbox, "")
+    userEvent.click(addButton)
+    await waitFor(() => {
+      expect(editor.children).toEqual(output.children)
+    })
   })
 })
