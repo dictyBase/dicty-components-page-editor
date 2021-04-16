6c1a226df13ac776d7ccb4b501c7c0f21fdc0711
wildlifehexagon <hartline.eric@gmail.com>
2021-04-16 11:18:54 -0500

    fix: add extra check for font size dropdown display

--- src/utils/getCurrentMark.ts
+++ src/utils/getCurrentMark.ts
@@ -1,11 +1,21 @@
 import { Editor } from "slate"
 
+// adds additional check so an inherited font size displays the default "1rem"
+// in the dropdown menu
+const getMarkValue = (value: string) => {
+  if (value === "inherit") {
+    return "1rem"
+  }
+  return value
+}
+
 // get the current mark for a given selection
 const getCurrentMark = (editor: Editor, mark: string) => {
   const marks = Editor.marks(editor)
-  if (marks && marks[mark]) {
-    return marks[mark]
+  if (!marks || !marks[mark]) {
+    return
   }
+  return getMarkValue(marks[mark])
 }
 
 export default getCurrentMark
