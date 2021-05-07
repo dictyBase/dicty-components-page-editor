c14b4f20eef98c8cadc4829d524338b9f63e49a9
wildlifehexagon <hartline.eric@gmail.com>
2021-05-07 14:29:31 -0500

    style: add note about inline nodes

--- src/plugins/withLinks.ts
+++ src/plugins/withLinks.ts
@@ -5,6 +5,8 @@ const withLinks = (editor: Editor) => {
   const { isInline } = editor
 
   // make sure every link type is rendered as inline
+  // NOTE: if a node is inline then it automatically adds children with { text: "" }
+  // https://docs.slatejs.org/concepts/02-nodes
   editor.isInline = (element) => {
     if (element.type === types.link) {
       return true
