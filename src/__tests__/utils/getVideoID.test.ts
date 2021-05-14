198eec5aca30945538c90a0f09da08b01b26ef60
wildlifehexagon <hartline.eric@gmail.com>
2021-05-14 14:11:33 -0500

    refactor: move id checks into same function

--- src/__tests__/utils/getVideoID.test.ts
+++ src/__tests__/utils/getVideoID.test.ts
@@ -1,4 +1,4 @@
-import getVideoID, { getVimeoID, getYouTubeID } from "../../utils/getVideoID"
+import getVideoID from "../../utils/getVideoID"
 
 const alertMock = jest.fn()
 global.alert = alertMock
@@ -18,19 +18,5 @@ describe("getVideoID function", () => {
     const fakeURL = "https://dictycr.org/notavideo"
     expect(getVideoID(fakeURL)).toEqual(fakeURL)
     expect(alertMock).toHaveBeenCalled()
-  })
-})
-
-describe("getYoutubeID function", () => {
-  it("should handle standard URL", () => {
-    expect(getYouTubeID("https://www.youtube.com/watch?v=2hp14nY-qh0")).toEqual(
-      "2hp14nY-qh0",
-    )
-  })
-})
-
-describe("getVimeoID function", () => {
-  it("should handle standard URL", () => {
-    expect(getVimeoID("https://vimeo.com/11653518")).toEqual("11653518")
   })
 })
--- src/utils/getVideoID.ts
+++ src/utils/getVideoID.ts
@@ -1,33 +1,20 @@
-const getYouTubeID = (url: string) => {
-  const match = url.match(
-    /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/+|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/i,
-  )
-  if (match && match.length > 0) {
-    return match[1]
-  }
-  return url
-}
-
-const getVimeoID = (url: string) => {
-  const match = url.match(
-    /\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/(?:[^/]*)\/videos\/|album\/(?:\d+)\/video\/|video\/|)(\d+)(?:[a-zA-Z0-9_-]+)?/i,
-  )
-  if (match && match.length > 0) {
-    return match[1]
-  }
-  return url
-}
+const youTubeRegex = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/+|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/i
+const vimeoRegex = /\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/(?:[^/]*)\/videos\/|album\/(?:\d+)\/video\/|video\/|)(\d+)(?:[a-zA-Z0-9_-]+)?/i
 
 const getVideoID = (url: string) => {
+  let match
   if (url.includes("youtube")) {
-    return getYouTubeID(url)
+    match = url.match(youTubeRegex)
   }
   if (url.includes("vimeo")) {
-    return getVimeoID(url)
+    match = url.match(vimeoRegex)
   }
+  if (match && match.length > 0) {
+    return match[1]
+  }
+
   alert("Can only accept YouTube or Vimeo URL")
   return url
 }
 
-export { getYouTubeID, getVimeoID }
 export default getVideoID
