838a8eb6e2ec407a7a871c897e2df4230d89d08d
wildlifehexagon <hartline.eric@gmail.com>
2021-06-03 20:02:14 -0500

    refactor: separate logic inside lists plugin

--- src/plugins/withLists.ts
+++ src/plugins/withLists.ts
@@ -34,6 +34,58 @@ const liftNodes = (editor: Editor) => {
   }
 }
 
+const handleInsertBreak = (editor: Editor, insertBreak: () => void) => {
+  // this plugin only applies custom logic to active lists
+  if (isActiveList(editor)) {
+    const match = listItemMatch(editor)
+
+    if (match) {
+      const [, path] = match
+      const text = Editor.string(editor, path)
+
+      // if the current selection's text is empty, unwrap the list node and add
+      // an empty paragraph instead
+      if (text === "") {
+        Transforms.unwrapNodes(editor, {
+          match: (n) => listTypes.includes(n.type as string),
+          // split is needed to unwrap this selection from the list type
+          split: true,
+        })
+        Transforms.setNodes(editor, {
+          type: types.paragraph,
+        })
+      } else {
+        insertBreak()
+      }
+    }
+  } else {
+    // if not a list, just insert break as normal
+    insertBreak()
+  }
+}
+
+const handleListEscaping = (editor: Editor) => {
+  const { selection } = editor
+
+  // check that there is a current selection without highlight
+  if (selection && Range.isCollapsed(selection)) {
+    const match = listItemMatch(editor)
+
+    if (match) {
+      const [, path] = match
+      const text = Editor.string(editor, path)
+      const start = Editor.start(editor, path)
+
+      if (text === "") {
+        // verify the start of the selection and the anchor are equal
+        if (Point.equals(selection.anchor, start)) {
+          undentItem(editor)
+        }
+      }
+    }
+  }
+}
+
 /**
  * withLists modifies the logic for inserting a line break inside lists.
  */
@@ -41,55 +93,11 @@ const withLists = (editor: Editor) => {
   const { insertBreak, deleteBackward } = editor
 
   editor.insertBreak = () => {
-    // this plugin only applies custom logic to active lists
-    if (isActiveList(editor)) {
-      const match = listItemMatch(editor)
-
-      if (match) {
-        const [, path] = match
-        const text = Editor.string(editor, path)
-
-        // if the current selection's text is empty, unwrap the list node and add
-        // an empty paragraph instead
-        if (text === "") {
-          Transforms.unwrapNodes(editor, {
-            match: (n) => listTypes.includes(n.type as string),
-            // split is needed to unwrap this selection from the list type
-            split: true,
-          })
-          Transforms.setNodes(editor, {
-            type: types.paragraph,
-          })
-        } else {
-          insertBreak()
-        }
-      }
-    } else {
-      // if not a list, just insert break as normal
-      insertBreak()
-    }
+    handleInsertBreak(editor, insertBreak)
   }
 
   editor.deleteBackward = (...args) => {
-    const { selection } = editor
-
-    // check that there is a current selection without highlight
-    if (selection && Range.isCollapsed(selection)) {
-      const match = listItemMatch(editor)
-
-      if (match) {
-        const [, path] = match
-        const text = Editor.string(editor, path)
-        const start = Editor.start(editor, path)
-
-        if (text === "") {
-          // verify the start of the selection and the anchor are equal
-          if (Point.equals(selection.anchor, start)) {
-            undentItem(editor)
-          }
-        }
-      }
-    }
+    handleListEscaping(editor)
     deleteBackward(...args)
   }
 
