b047f0bb6d0b1292718a149e18388c8f35a74ce0
wildlifehexagon <hartline.eric@gmail.com>
2021-06-11 13:57:50 -0500

    fix: clean up logic in lists plugin

--- src/plugins/withLists.ts
+++ src/plugins/withLists.ts
@@ -1,104 +1,95 @@
-import { Editor, Element, Point, Range, Transforms } from "slate"
+import { Editor, Element, Node, Point, Range, Transforms } from "slate"
 import { types } from "../constants/types"
 
-const listTypes = [types.orderedList, types.unorderedList]
+// helper function to check for ul/ol match
+const parentListMatch = (n: Node) =>
+  (!Editor.isEditor(n) &&
+    Element.isElement(n) &&
+    n.type === types.orderedList) ||
+  (!Editor.isEditor(n) &&
+    Element.isElement(n) &&
+    n.type === types.unorderedList)
 
-const listItemMatch = (editor: Editor) => {
+// helper function to check for li match
+const listItemMatch = (n: Node) =>
+  !Editor.isEditor(n) && Element.isElement(n) && n.type === types.listItem
+
+// helper function to get matching ul/ol node
+const findMatchingList = (editor: Editor) => {
+  const [listMatch] = Array.from(
+    Editor.nodes(editor, {
+      match: (n) => parentListMatch(n),
+    }),
+  )
+  return listMatch
+}
+
+// helper function to get matching li node
+const findMatchingListItem = (editor: Editor) => {
   const [match] = Array.from(
     Editor.nodes(editor, {
-      match: (n) =>
-        !Editor.isEditor(n) &&
-        Element.isElement(n) &&
-        n.type === types.listItem,
+      match: (n) => listItemMatch(n),
     }),
   )
   return match
 }
 
-const listMatch = (editor: Editor) => {
-  const [match] = Array.from(
-    Editor.nodes(editor, {
-      mode: "lowest",
-      match: (n) =>
-        (!Editor.isEditor(n) &&
-          Element.isElement(n) &&
-          n.type === types.orderedList) ||
-        (!Editor.isEditor(n) &&
-          Element.isElement(n) &&
-          n.type === types.unorderedList),
-    }),
-  )
-
-  return match
-}
-
+// helper function called when user escapes out of list
 const liftNodes = (editor: Editor) => {
-  const match = listMatch(editor)
+  // check for new parent
+  const listMatch = findMatchingList(editor)
   // verify there is an active list to lift the nodes
-  if (match) {
+  if (listMatch) {
+    // lift the list item node to next parent
     Transforms.liftNodes(editor, {
-      match: (n) =>
-        !Editor.isEditor(n) &&
-        Element.isElement(n) &&
-        n.type === types.listItem,
+      match: (n) => listItemMatch(n),
     })
   }
 }
 
-const handleInsertBreak = (editor: Editor, insertBreak: () => void) => {
-  const { selection } = editor
-  // this plugin only applies custom logic to active lists
-  if (selection && Range.isCollapsed(selection)) {
-    const match = listItemMatch(editor)
-
-    if (match) {
-      const [, path] = match
-      const text = Editor.string(editor, path)
-
-      // if the current selection's text is empty, unwrap the list node and add
-      // an empty paragraph instead
-      if (text === "") {
-        Transforms.unwrapNodes(editor, {
-          match: (n) =>
-            !Editor.isEditor(n) &&
-            Element.isElement(n) &&
-            listTypes.includes(n.type as string),
-          // split is needed to unwrap this selection from the list type
-          split: true,
-        })
-        Transforms.setNodes(editor, {
-          type: types.paragraph,
-        })
-      } else {
-        insertBreak()
-      }
-    }
-  } else {
-    // if not a list, just insert break as normal
-    insertBreak()
-  }
-}
-
-const handleListEscaping = (editor: Editor) => {
+const handleLists = (editor: Editor, callback: () => void) => {
   const { selection } = editor
 
   // check that there is a current selection without highlight
   if (selection && Range.isCollapsed(selection)) {
-    const match = listItemMatch(editor)
+    // find the closest list item element
+    const [match] = Array.from(
+      Editor.nodes(editor, {
+        match: (n) =>
+          listItemMatch(n) &&
+          n.children &&
+          n.children[0] &&
+          (!n.children[0].text || n.children[0].text === ""),
+      }),
+    )
 
+    // check that there was a match
     if (match) {
       const [, path] = match
-      const text = Editor.string(editor, path)
       const start = Editor.start(editor, path)
 
-      if (text === "") {
-        // verify the start of the selection and the anchor are equal
-        if (Point.equals(selection.anchor, start)) {
-          undentItem(editor)
+      // if the selection is at the beginning of the list item
+      if (Point.equals(selection.anchor, start)) {
+        // lift the list item to the next parent
+        liftNodes(editor)
+        // check for the new parent
+        const listMatch = findMatchingList(editor)
+        // if it is no longer within a ul/ol, turn into normal paragraph
+        if (!listMatch) {
+          Transforms.setNodes(
+            editor,
+            { type: types.paragraph },
+            {
+              match: (n) => listItemMatch(n),
+            },
+          )
         }
+        return
       }
     }
   }
+
+  callback()
 }
 
 /**
@@ -108,12 +99,11 @@ const withLists = (editor: Editor) => {
   const { insertBreak, deleteBackward } = editor
 
   editor.insertBreak = () => {
-    handleInsertBreak(editor, insertBreak)
+    handleLists(editor, insertBreak)
   }
 
   editor.deleteBackward = (...args) => {
-    handleListEscaping(editor)
-    deleteBackward(...args)
+    handleLists(editor, () => deleteBackward(...args))
   }
 
   return editor
@@ -124,23 +114,29 @@ const indentItem = (editor: Editor) => {
 
   // check that there is a current selection without highlight
   if (selection && Range.isCollapsed(selection)) {
-    const match = listItemMatch(editor)
+    const match = findMatchingListItem(editor)
 
     if (match) {
-      const parentMatch = listMatch(editor)
+      const [listMatch] = Array.from(
+        Editor.nodes(editor, {
+          mode: "lowest",
+          match: (n) => parentListMatch(n),
+        }),
+      )
 
-      if (parentMatch) {
-        let depth = parentMatch[1].length
+      if (listMatch) {
+        let depth = listMatch[1].length
         if (depth <= 5) {
-          if (Element.isElement(parentMatch[0])) {
+          if (Element.isElement(listMatch[0])) {
             Transforms.wrapNodes(editor, {
-              type: parentMatch[0].type,
+              type: listMatch[0].type,
               children: [],
             })
           }
         }
       }
     } else {
+      // if the user is hitting tab and not inside a list, insert spaces
       editor.insertText("    ")
     }
   }
@@ -149,26 +145,20 @@ const indentItem = (editor: Editor) => {
 const undentItem = (editor: Editor) => {
   const { selection } = editor
 
-  // check that there is a current selection without highlight
   if (selection && Range.isCollapsed(selection)) {
-    const match = listItemMatch(editor)
+    const match = findMatchingListItem(editor)
 
     if (match) {
-      // 'lift' the list-item to the next parent
       liftNodes(editor)
-      // check for the new parent
-      const parentMatch = listMatch(editor)
+      const listMatch = findMatchingList(editor)
 
       // if it is no longer within an active list, turn into paragraph
-      if (!parentMatch) {
+      if (!listMatch) {
         Transforms.setNodes(
           editor,
-          { type: "paragraph" },
+          { type: types.paragraph },
           {
-            match: (n) =>
-              !Editor.isEditor(n) &&
-              Element.isElement(n) &&
-              n.type === types.listItem,
+            match: (n) => listItemMatch(n),
           },
         )
       }
