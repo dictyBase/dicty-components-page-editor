b67c3e0616f30c2b94890f371861d7e2b2a1e144
wildlifehexagon <hartline.eric@gmail.com>
2021-06-18 17:31:35 -0500

    refactor: limit conditional nesting

--- src/utils/tables.ts
+++ src/utils/tables.ts
@@ -53,72 +53,73 @@ const insertTable = (editor: Editor) => {
 
 const insertTableRow = (editor: Editor) => {
   const currentRow = getNodeAbove(editor, types.tableRow)
-
-  if (currentRow) {
-    const [, rowPath] = currentRow
-    const [tableNode, tablePath] = Editor.parent(editor, rowPath)
-    if (!SlateElement.isElement(tableNode)) {
-      return
-    }
-
-    const col = tableNode.col as number
-    const row = tableNode.row as number
-
-    Transforms.insertNodes(editor, getEmptyTableRow(col), {
-      at: Path.next(rowPath),
-    })
-
-    Transforms.setNodes(editor, { row: row + 1 }, { at: tablePath })
+  if (currentRow === undefined) {
+    return
   }
+
+  const [, rowPath] = currentRow
+  const [tableNode, tablePath] = Editor.parent(editor, rowPath)
+  if (!SlateElement.isElement(tableNode)) {
+    return
+  }
+
+  const col = tableNode.col as number
+  const row = tableNode.row as number
+
+  Transforms.insertNodes(editor, getEmptyTableRow(col), {
+    at: Path.next(rowPath),
+  })
+
+  Transforms.setNodes(editor, { row: row + 1 }, { at: tablePath })
 }
 
 const insertTableColumn = (editor: Editor) => {
   const currentCell = getNodeAbove(editor, types.tableCell)
-
-  if (currentCell) {
-    const currentTable = getNodeAbove(editor, types.table)
-    if (currentTable === undefined) {
-      return
-    }
-
-    const [, cellPath] = currentCell
-    const [tableNode, tablePath] = currentTable
-
-    // get next item in path where new cell should be located
-    const nextCellPath = Path.next(cellPath)
-    // get copy of next cell path to change index later
-    const newCellPath = nextCellPath.slice()
-    // find path position to enter new cell
-    const replacePathPos = newCellPath.length - 2
-    // get index of current row using this path position
-    const currentRowIndex = newCellPath[replacePathPos]
-
-    if (!SlateElement.isElement(tableNode)) {
-      return
-    }
-
-    // loop over table node and insert empty cells at given path
-    tableNode.children.forEach(
-      // @ts-ignore <-- used because we don't need the first val here
-      (_, index: number) => {
-        // replace path position in new cell path with the current index
-        newCellPath[replacePathPos] = index
-        // insert an empty table cell at this position
-        Transforms.insertNodes(editor, getEmptyTableCell(), {
-          at: newCellPath,
-          select: index === currentRowIndex,
-        })
-      },
-    )
-    // update col attribute for table node
-    Transforms.setNodes(
-      editor,
-      { col: Number(tableNode.col) + 1 },
-      {
-        at: tablePath,
-      },
-    )
+  if (currentCell === undefined) {
+    return
   }
+  const currentTable = getNodeAbove(editor, types.table)
+  if (currentTable === undefined) {
+    return
+  }
+
+  const [, cellPath] = currentCell
+  const [tableNode, tablePath] = currentTable
+
+  // get next item in path where new cell should be located
+  const nextCellPath = Path.next(cellPath)
+  // get copy of next cell path to change index later
+  const newCellPath = nextCellPath.slice()
+  // find path position to enter new cell
+  const replacePathPos = newCellPath.length - 2
+  // get index of current row using this path position
+  const currentRowIndex = newCellPath[replacePathPos]
+
+  if (!SlateElement.isElement(tableNode)) {
+    return
+  }
+
+  // loop over table node and insert empty cells at given path
+  tableNode.children.forEach(
+    // @ts-ignore <-- used because we don't need the first val here
+    (_, index: number) => {
+      // replace path position in new cell path with the current index
+      newCellPath[replacePathPos] = index
+      // insert an empty table cell at this position
+      Transforms.insertNodes(editor, getEmptyTableCell(), {
+        at: newCellPath,
+        select: index === currentRowIndex,
+      })
+    },
+  )
+  // update col attribute for table node
+  Transforms.setNodes(
+    editor,
+    { col: Number(tableNode.col) + 1 },
+    {
+      at: tablePath,
+    },
+  )
 }
 
 const deleteTable = (editor: Editor) => {
@@ -133,83 +134,83 @@ const deleteTable = (editor: Editor) => {
 
 const deleteTableRow = (editor: Editor) => {
   const currentRow = getNodeAbove(editor, types.tableRow)
-
-  if (currentRow) {
-    const [, rowPath] = currentRow
-    const [tableNode, tablePath] = Editor.parent(editor, rowPath)
-
-    if (!SlateElement.isElement(tableNode)) {
-      return
-    }
-
-    const row = Number(tableNode.row)
-
-    // if there's only one row in the table then safely remove the table
-    if (row === 1) {
-      deleteTable(editor)
-      return
-    }
-    // otherwise remove the nodes at the row path and then set table node's
-    // row attribute to one less
-    Transforms.removeNodes(editor, {
-      at: rowPath,
-    })
-    Transforms.setNodes(
-      editor,
-      {
-        row: row - 1,
-      },
-      {
-        at: tablePath,
-      },
-    )
+  if (currentRow === undefined) {
+    return
   }
+
+  const [, rowPath] = currentRow
+  const [tableNode, tablePath] = Editor.parent(editor, rowPath)
+
+  if (!SlateElement.isElement(tableNode)) {
+    return
+  }
+
+  const row = Number(tableNode.row)
+
+  // if there's only one row in the table then safely remove the table
+  if (row === 1) {
+    deleteTable(editor)
+    return
+  }
+  // otherwise remove the nodes at the row path and then set table node's
+  // row attribute to one less
+  Transforms.removeNodes(editor, {
+    at: rowPath,
+  })
+  Transforms.setNodes(
+    editor,
+    {
+      row: row - 1,
+    },
+    {
+      at: tablePath,
+    },
+  )
 }
 
 const deleteTableColumn = (editor: Editor) => {
   const currentCell = getNodeAbove(editor, types.tableCell)
-
   if (currentCell === undefined) {
     return
   }
-
   const currentTable = getNodeAbove(editor, types.table)
-
-  if (currentTable) {
-    const [, cellPath] = currentCell
-    const [tableNode, tablePath] = currentTable
-
-    if (!SlateElement.isElement(tableNode)) {
-      return
-    }
-
-    const col = Number(tableNode.col)
-
-    if (col === 1) {
-      deleteTable(editor)
-      return
-    }
-
-    const pathToDelete = cellPath.slice()
-    const replacePathPos = pathToDelete.length - 2
-    // @ts-ignore <-- use to ignore unneeded value
-    tableNode.children.forEach((_, index: number) => {
-      pathToDelete[replacePathPos] = index
-      Transforms.removeNodes(editor, {
-        at: pathToDelete,
-      })
-    })
-
-    Transforms.setNodes(
-      editor,
-      {
-        col: col - 1,
-      },
-      {
-        at: tablePath,
-      },
-    )
+  if (currentTable === undefined) {
+    return
   }
+
+  const [, cellPath] = currentCell
+  const [tableNode, tablePath] = currentTable
+
+  if (!SlateElement.isElement(tableNode)) {
+    return
+  }
+
+  const col = Number(tableNode.col)
+
+  if (col === 1) {
+    deleteTable(editor)
+    return
+  }
+
+  const pathToDelete = cellPath.slice()
+  const replacePathPos = pathToDelete.length - 2
+  // @ts-ignore <-- use to ignore unneeded value
+  tableNode.children.forEach((_, index: number) => {
+    pathToDelete[replacePathPos] = index
+    Transforms.removeNodes(editor, {
+      at: pathToDelete,
+    })
+  })
+
+  Transforms.setNodes(
+    editor,
+    {
+      col: col - 1,
+    },
+    {
+      at: tablePath,
+    },
+  )
 }
 
 export {
