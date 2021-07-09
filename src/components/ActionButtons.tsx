b4c1f59671929bbc7369285302a442e052271d52
wildlifehexagon <hartline.eric@gmail.com>
2021-07-09 14:58:38 -0500

    refactor: make reusable ActionButton component

--- src/components/ActionButtons.tsx
+++ src/components/ActionButtons.tsx
@@ -1,15 +1,6 @@
 import React from "react"
-import { makeStyles, Theme } from "@material-ui/core/styles"
 import Box from "@material-ui/core/Box"
-import Button from "@material-ui/core/Button"
-
-const useStyles = makeStyles((theme: Theme) => ({
-  button: {
-    minWidth: "70px",
-    textTransform: "none",
-    marginRight: theme.spacing(1),
-  },
-}))
+import ActionButton from "./buttons/ActionButton"
 
 type Props = {
   /** Function called when cancel button is clicked */
@@ -22,25 +13,10 @@ type Props = {
  * ActionButtons contains the cancel and save buttons used at the bottom of the editor.
  */
 const ActionButtons = ({ handleCancel, handleSave }: Props) => {
-  const classes = useStyles()
-
   return (
     <Box display="flex" flexDirection="row" justifyContent="flex-end">
-      <Button
-        className={classes.button}
-        size="small"
-        variant="contained"
-        onClick={handleCancel}>
-        Cancel
-      </Button>
-      <Button
-        className={classes.button}
-        size="small"
-        variant="contained"
-        color="primary"
-        onClick={handleSave}>
-        Save
-      </Button>
+      <ActionButton handleClick={handleCancel} text="Cancel" />
+      <ActionButton handleClick={handleSave} text="Save" color="primary" />
     </Box>
   )
 }
