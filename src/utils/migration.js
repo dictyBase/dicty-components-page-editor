0072cf29652531b9359b00fbd2c179dda4149925
wildlifehexagon <hartline.eric@gmail.com>
2021-07-12 15:39:57 -0500

    feat: add data migration script

---
+++ src/utils/migration.js
@@ -0,0 +1,210 @@
+// the two font arrays are taken from the old page editor and used to convert old data
+const FontFamilyList = [
+  { name: "Lato" },
+  { name: "Merriweather" },
+  { name: "Montserrat" },
+  { name: "Roboto" },
+  { name: "Roboto Condensed" },
+  { name: "Roboto Mono" },
+  { name: "Roboto Slab" },
+]
+
+const FontSizeList = [
+  { size: "12px" },
+  { size: "14px" },
+  { size: "16px" },
+  { size: "18px" },
+  { size: "20px" },
+  { size: "22px" },
+  { size: "26px" },
+  { size: "30px" },
+]
+
+/**
+ * convertData receives a node object and converts its nested
+ * 'data' object into the new Slate format.
+ */
+const convertData = (node) => {
+  const { type } = node
+  switch (type) {
+    case "alignment":
+      return {
+        align: node.data["align"],
+      }
+    case "image":
+      return {
+        url: node.data["src"],
+        description: node.data["description"],
+        height: node.data["height"],
+        width: node.data["width"],
+      }
+    case "line-spacing":
+      return {
+        lineSpacing: node.data["size"],
+      }
+    case "link":
+      return {
+        url: node.data["href"],
+      }
+    case "video":
+      return {
+        url: node.data["url"],
+        height: node.data["height"],
+        width: node.data["width"],
+      }
+    default:
+      return {}
+  }
+}
+
+/**
+ * convertType converts an existing 'type' property into the 'type'
+ * used by the new version of the editor.
+ */
+const convertType = (type) => {
+  let convertedType = ""
+  switch (type) {
+    case "line-spacing":
+      convertedType = "lineSpacing"
+      break
+    case "ordered-list":
+      convertedType = "orderedList"
+      break
+    case "unordered-list":
+      convertedType = "unorderedList"
+      break
+    case "list-item":
+      convertedType = "listItem"
+      break
+    case "table":
+      convertedType = "tableWrap"
+      break
+    case "table-row":
+      convertedType = "tableRow"
+      break
+    case "table-cell":
+      convertedType = "tableCell"
+      break
+    default:
+      convertedType = type
+  }
+  return convertedType
+}
+
+const convertChildren = (node) => {
+  // if there are nodes then convert the children
+  if (node.nodes) {
+    return node.nodes.reduce((acc, val) => {
+      const nodes = convertNode(val)
+
+      // if the converted current value is an array, only grab the object inside of it
+      if (Array.isArray(nodes)) {
+        return [...acc, ...nodes]
+      }
+      // otherwise add the new value in its existing object form
+      return [...acc, nodes]
+    }, [])
+  }
+  // otherwise include mandatory object with text property
+  return [{ text: "" }]
+}
+
+const convertNode = (node) => {
+  const { type } = node
+  if (type) {
+    // remove any alignment wrappers from old structure;
+    // previously, changing the alignment would add a new <div> around the selection
+    if (type === "alignment") {
+      return {
+        ...convertChildren(node)[0],
+        ...convertData(node),
+      }
+    }
+
+    return {
+      type: convertType(type),
+      children: convertChildren(node),
+      ...convertData(node),
+    }
+  }
+
+  const { text, marks, leaves } = node
+
+  /**
+   * Leaves is an array containing leaf objects of this structure:
+   * {
+   *  object: "leaf",
+   *  text: "george costanza",
+   *  marks: [
+   *    {
+   *      object: "mark",
+   *      type: "italic",
+   *      data: {}
+   *    }
+   *  ]
+   * }
+   *
+   * Each leaf node needs to be converted recursively.
+   */
+  if (leaves) {
+    return [...leaves.map(convertNode)]
+  }
+
+  /**
+    Example node to check for:
+      {
+        object: "leaf",
+        text: "periodically",
+        marks: [
+          {
+            object: "mark",
+            type: "italic",
+            data: {},
+          },
+        ],
+      }
+    
+  */
+  if (marks && marks.length > 0) {
+    // return object with text and list of marks with appropriate values
+    return {
+      text,
+      ...marks.reduce((acc, mark) => {
+        if (mark.type === "font-color") {
+          return {
+            ...acc,
+            fontColor: mark.data.color,
+          }
+        }
+        if (mark.type === "font-family") {
+          return {
+            ...acc,
+            fontFamily: FontFamilyList[mark.data.fontFamilyIndex],
+          }
+        }
+        if (mark.type === "font-size") {
+          return {
+            ...acc,
+            fontSize: FontSizeList[mark.data.fontSizeIndex],
+          }
+        }
+
+        return {
+          ...acc,
+          [mark.type]: true,
+        }
+      }, {}),
+    }
+  }
+
+  // if no leaves or marks then just return plain text
+  return {
+    text,
+  }
+}
+
+const convertSlate047 = (object) => {
+  const { nodes } = object.document
+
+  return nodes.map(convertNode)
+}
