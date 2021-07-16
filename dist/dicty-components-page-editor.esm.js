da5f42ae65748b77f073b20717f576d89f16f00a
wildlifehexagon <hartline.eric@gmail.com>
2021-07-16 12:12:11 -0500

    fix: use if/else block

--- dist/dicty-components-page-editor.esm.js
+++ dist/dicty-components-page-editor.esm.js
@@ -3228,9 +3228,10 @@ var convertSlate047 = function convertSlate047(object) {
 
   if (Array.isArray(convertedNodes[0])) {
     newNodes = removeEmptyObjects(convertedNodes[0]);
+  } else {
+    newNodes = removeEmptyObjects(convertedNodes);
   }
 
-  newNodes = removeEmptyObjects(convertedNodes);
   return newNodes.flat();
 };
 
