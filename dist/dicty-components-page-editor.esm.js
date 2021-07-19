5aae06105a9ea5e4aada62a7922141d0f9363cfe
wildlifehexagon <hartline.eric@gmail.com>
2021-07-19 09:55:06 -0500

    feat: remove extra console.log
    BREAKING CHANGE: This is the correct major release for the new Slate editor using the 0.5x data format.

--- dist/dicty-components-page-editor.esm.js
+++ dist/dicty-components-page-editor.esm.js
@@ -3308,7 +3308,6 @@ var PageEditor = function PageEditor(_ref) {
   };
 
   var toolbar = inline ? React.createElement(InlineToolbar, null) : React.createElement(EditorToolbar, null);
-  console.log(value);
   return React.createElement(ThemeProvider, {
     theme: theme
   }, React.createElement(Slate, {
--- src/components/PageEditor.tsx
+++ src/components/PageEditor.tsx
@@ -91,7 +91,7 @@ const PageEditor = ({
   }
 
   let toolbar = inline ? <InlineToolbar /> : <Toolbar />
-  console.log(value)
+
   return (
     <ThemeProvider theme={theme}>
       <Slate
