501f9b39c25e41c1966671df65ac347d411c447e
wildlifehexagon <hartline.eric@gmail.com>
2021-07-20 11:35:23 -0500

    feat: remove data migration script
    BREAKING CHANGE: Slate 0.47 data is no longer converted on the fly.

--- src/__tests__/utils/migration.test.ts
+++
@@ -1,9 +0,0 @@
-import convertSlate047 from "../../utils/migration"
-import { oldContent, newContent } from "./migrationMockData"
-
-describe("data migration", () => {
-  it("should convert old data to new format", () => {
-    const converted = convertSlate047(oldContent)
-    expect(converted).toEqual(newContent)
-  })
-})
